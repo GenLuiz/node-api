7b39ea85daeedda3da0bc25dca8f8ef7aba563d5
Luiz Fernando <luizfernandobarbosa@hotmail.com>
2018-10-17 00:54:33 -0300

    reestructure

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
-
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+//Routes
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
 
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'reactive native',
-        description: 'test mongo db',
-        url: 'https://www.google.com'
-    })
-    res.send("Hello world");
-});
