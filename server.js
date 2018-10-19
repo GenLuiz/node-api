9bf2122c0d0332d066eaad57d96e7dd88b469fd7
Luiz Barbosa <e077272@mastercard.com>
2018-10-19 16:37:39 -0300

    adding post method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const app = express();
+app.use(express.json());
 
 mongoose.connect('mongodb://10.157.138.65:27017/nodeapi', {useNewUrlParser: true});
 
